9ce1bcd6be7cb07d3a687ee58a72468cea3d1908
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-05-26 00:00:25 +0530

    Added new SW prog

--- 19_Sliding Window and Two Pointers/Prog07_No_of_Substrings_Containing_All_Three_Characters.java
+++ 19_Sliding Window and Two Pointers/Prog07_No_of_Substrings_Containing_All_Three_Characters.java
@@ -40,7 +40,7 @@ public class Prog07_No_of_Substrings_Containing_All_Three_Characters {
             lastSeen[str.charAt(i)-'a'] = i;
             if(lastSeen[0]!=-1 && lastSeen[1]!=-1 && lastSeen[2]!=-1){
                 int minLastSeen = Math.min(lastSeen[0],Math.min(lastSeen[1],lastSeen[2]));
-                
+                cnt+=minLastSeen+1;
             }
         }
         return cnt;
