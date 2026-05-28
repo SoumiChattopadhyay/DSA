a60001b41ef01ff915e62b1f9069ffa4c6a265aa
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-05-29 00:21:46 +0530

    Modified SW prog

--- 19_Sliding Window and Two Pointers/Prog09_Binary_Subarrays_with_Sum_Equal_To_Goal.java
+++ 19_Sliding Window and Two Pointers/Prog09_Binary_Subarrays_with_Sum_Equal_To_Goal.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Prog09_Binary_Subarrays_with_Sum_Equal_To_Goal {
     // Optimal
     static int funct3(int[] arr, int goal){
@@ -19,10 +22,61 @@ public class Prog09_Binary_Subarrays_with_Sum_Equal_To_Goal {
         }
         return cnt;
     }
+
+     // Brute
+    static int funct1(int[] arr, int K){
+        int n = arr.length;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int sum=0;
+                for(int k=i;k<=j;k++){
+                    sum+=arr[k];
+                }
+                if(sum==K){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+    // Better Brute
+    static int funct1_2(int[] arr, int k){
+        int n = arr.length;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=arr[j];
+                if(sum==k){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+    // Better
+    static int funct2(int[] arr, int k){
+        int n=arr.length;
+        Map<Integer,Integer> mp = new HashMap<>();
+        mp.put(0,1);
+        int prefixSum=0;
+        int cnt=0;//will store all the subarrays with sum k in the entire array
+        for(int i=0;i<n;i++){
+            prefixSum+=arr[i];
+            int remove = prefixSum-k;
+            cnt+=mp.getOrDefault(remove, 0);//how many times prefixSum-k to be removed occurs = no. of times the subarray with sum k ending at idx i occurs. So add that to cnt which stores total no. of subarrays with sum k in the entire array
+            mp.put(prefixSum,mp.getOrDefault(prefixSum, 0)+1);//Put prefixSum and no. of times current prefixSum occurs. If it occured before increase the existing count else create a new key with cnt=1
+        }
+        return cnt;
+    }
     
     public static void main(String[] args) {
         int[] arr = {1,0,0,1,1,0};
         int goal=2;
         System.out.println(funct3(arr, goal));
+        System.out.println(funct1(arr, goal));
+        System.out.println(funct1_2(arr, goal));
+        System.out.println(funct2(arr, goal));
     }
 }
