2a29062c0c070c5148c3f3d4ace47174f7d5a1a8
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-05-28 15:15:59 +0530

    Modified SW prog

--- 19_Sliding Window and Two Pointers/Prog08_Longest_Repeating_Character_Replacement.java
+++ 19_Sliding Window and Two Pointers/Prog08_Longest_Repeating_Character_Replacement.java
@@ -20,16 +20,36 @@ public class Prog08_Longest_Repeating_Character_Replacement {
         return maxLen;
     }
     // Better
-    // static int funct2(String str, int k){
-    //     int n = str.length();
-    //     int l=0,r=0,maxLen=0;
-    //     while(r<n){
-            
-    //     }
-    // }
+    static int funct2(String str, int k){
+        int n = str.length();
+        int hash[] = new int[26];
+        int l=0,r=0,maxf=0,maxLen=0;
+        while(r<n){
+            hash[str.charAt(r)-'A']++;
+            maxf = Math.max(maxf,hash[str.charAt(r)-'A']);
+            int len = r-l+1;
+            int changes = len - maxf;
+            while(changes>k){
+                hash[str.charAt(l)-'A']--;
+                maxf=0;
+                for(int i=0;i<26;i++){
+                    maxf = Math.max(maxf,hash[i]);
+                }
+                len = r-l+1;
+                changes = len - maxf;
+                l++;
+            }
+            if(changes<=k){
+                maxLen = Math.max(maxLen,len);
+            }
+            r++;
+        }
+        return maxLen;
+    }
     public static void main(String[] args) {
         String str = "AABABBA";
         int k=2;
         System.out.println(funct1(str, k));
+        System.out.println(funct2(str, k));
     }
 }
