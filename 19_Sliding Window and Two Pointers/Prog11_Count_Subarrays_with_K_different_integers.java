ac7340a53b3b8710a67485c37f1201d25df3cfdb
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-05-29 22:54:09 +0530

    Added new SW prog

--- 19_Sliding Window and Two Pointers/Prog11_Count_Subarrays_with_K_different_integers.java
+++ 19_Sliding Window and Two Pointers/Prog11_Count_Subarrays_with_K_different_integers.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Prog11_Count_Subarrays_with_K_different_integers {
+    // Brute
     static int funct1(int[] arr, int k){
         int n=arr.length;
         int cnt=0;
@@ -19,9 +20,33 @@ public class Prog11_Count_Subarrays_with_K_different_integers {
         }
         return cnt;
     }
+    // Optimal
+    static int funct2(int[] arr, int k){
+        return funct(arr, k) - funct(arr, k-1);
+    }
+    // Counts no. of subarrays with different integers <= k
+    static int funct(int[] arr, int k){
+        int n = arr.length;
+        Map<Integer,Integer> map = new HashMap<>();
+        int l=0,r=0,cnt=0;
+        while(r<n){
+            map.put(arr[r], map.getOrDefault(arr[r], 0)+1);
+            while(map.size()>k){
+                map.put(arr[l],map.get(arr[l])-1);
+                if(map.get(arr[l])==0) map.remove(arr[l]);
+                l++;
+            }
+            if(map.size()<=k){
+                cnt+=(r-l+1);
+            }
+            r++;
+        }
+        return cnt;
+    }
     public static void main(String[] args) {
         int[] arr = {1,2,1,3,4};
         int k=3;
         System.out.println(funct1(arr, k));
+        System.out.println(funct2(arr, k));
     }
 }
