f615e75e8fd6251076bc95b37738f2acff9f524e
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-06-03 12:35:13 +0530

    Modified Greedy Program

--- 18_Greedy Algorithms/Programs/Prog10_Bag_of_Tokens.java
+++ 18_Greedy Algorithms/Programs/Prog10_Bag_of_Tokens.java
@@ -6,15 +6,15 @@ public class Prog10_Bag_of_Tokens{
         int score=0,maxScore=0;
         int left=0;
         int right=tokens.length-1;
-        while(left<=right){
-            // Case 1: Enough power to buy smallest token
+        while(left<=right){//Greedy approach : Use Power to play the smaller tokens, and use score to play the bigger tokens becoz we have a comdition that only when we have a power greater than tokens[i] we can play that token but for score there is no such restriction
+            // Case 1: Enough power to play smallest token
             if(power>=tokens[left]){
                 power-=tokens[left];//spend power
                 score++;//gain score
                 left++;//move left pointer
                 maxScore = Math.max(maxScore, score);//track maximum score
             }
-            // Case 2: Not enough power, sell biggest token
+            // Case 2: Not enough power, play biggest token
             else if(score>0){
                 power+=tokens[right];//gain power
                 score--;//lose one score
