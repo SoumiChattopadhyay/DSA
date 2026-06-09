9d4bee29776a99fd1a39ac32e9effc8c99f23e89
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-06-09 23:36:37 +0530

    Modified Binary Search programs

--- 04_Arrays and Binary Search/Binary Search/Prog08_rotationCount.java
+++ 04_Arrays and Binary Search/Binary Search/Prog08_rotationCount.java
@@ -6,7 +6,7 @@ public class Prog08_rotationCount {
     
         private static int countRotations(int[] arr) {
             int pivot = findPivot(arr);//el 7(at idx 3)
-            return pivot + 1;
+            return pivot + 1;//no. of rotations
         }
     
         // use this for non duplicates
@@ -16,15 +16,18 @@ public class Prog08_rotationCount {
             while (start<=end) {
                 int mid = start + (end - start) / 2;
                 // 4 cases over here
+                // Case 1: If (ele at mid) greater than (ele at mid+1) return mid
                 if (mid<end && arr[mid]>arr[mid+1]) {//since we are accessing arr[mid+1] so we must ensure mid is < end
                     return mid;
                 }
+                // Case 2: If (ele at mid) less than (ele at mid-1) return mid-1
                 if (mid>start && arr[mid]<arr[mid-1]) {
                     return mid-1;
                 }
-                if (arr[mid]<=arr[start]) {
+                // Case 3: If (ele at mid) less than (ele at start) or (ele at start) is (ele at mid), then we must go further left to find the pivot
+                if (arr[mid]<=arr[start]) {//Why is <= used instead of <? Because in a non-duplicate array: arr[mid] == arr[start] can only happen when: mid == start
                     end = mid-1;
-                } else {
+                } else {// Case 4: If (ele at mid) greater than (ele at start), then we must go further right to find the pivot
                     start = mid+1;
                 }
             }
@@ -36,38 +39,51 @@ public class Prog08_rotationCount {
             int start = 0;
             int end = arr.length - 1;
             while (start <= end) {
-                int mid = start + (end - start) / 2;
-                // 4 cases over here
-                if (mid < end && arr[mid] > arr[mid + 1]) {
+                int mid = start+(end-start)/2;
+                // 5 cases over here
+                // Case 1: If (ele at mid) greater than (ele at mid+1) return mid
+                if (mid<end && arr[mid]>arr[mid+1]) {
                     return mid;
                 }
-                if (mid > start && arr[mid] < arr[mid - 1]) {
+                // Case 2: If (ele at mid) less than (ele at mid-1) return mid-1
+                if (mid>start && arr[mid]<arr[mid-1]) {
                     return mid-1;
                 }
-    
-                // if elements at middle, start, end are equal then just skip the duplicates
+                // Case 3: If elements at middle, start, end are equal then just skip the duplicates
                 if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
                     // skip the duplicates
-                    // NOTE: what if these elements at start and end were the pivot??
-                    // check if start is pivot
-                    if (arr[start] > arr[start + 1]) {
+                    // NOTE: What if these elements at start and end-1 were the pivot?? Ex:[8,0,2,4,8,8,8] and [8,8,8,8,9,8]
+                    // check if start is pivot 
+                    if (arr[start] > arr[start+1]) {
                         return start;
                     }
-                    start++;
-    
-                    // check whether end is pivot
-                    if (arr[end] < arr[end - 1]) {
-                        return end - 1;
+                    start++;//skip duplicates 
+                    // check if end-1 is pivot
+                    if (arr[end] < arr[end-1]) {
+                        return end-1;
                     }
-                    end--;
+                    end--;//skip duplicates
                 }
-                // left side is sorted, so pivot should be in right
+                // condition to decide which side is definitely sorted, so we can search for the pivot in the other side.
+                // If elements at middle, start, end are not equal and if (ele at start) lesser than (ele at mid) or if (ele at end) lesser than (ele at mid)
+                // means left side is sorted(strictly increasing), so pivot should be in right
                 else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
+                    start = mid+1;
+                } else {//means I cannot conclude that the left side is sorted(strictly increasing) and the pivot is on the right side, so I'll search on the left side.
+                    end = mid-1;
                 }
             }
             return -1;
         }
-    }
+}
+/*
+arr[start] < arr[mid] case:
+    [4,5,6,7,0,1,2]
+     s   m
+So left side of mid is sorted(strictly increasing) so we must search for pivot in the right side of mid
+
+arr[start] == arr[mid] && arr[mid] > arr[end] case:
+    [3,3,3,4,5,1,2]
+    s   m        e
+Since the end value is smaller, rotation must have happened(pivot must be present) somewhere in the right side of mid.
+*/
--- 04_Arrays and Binary Search/Binary Search/Prog20_Book_Allocation_Problem.java
+++ 04_Arrays and Binary Search/Binary Search/Prog20_Book_Allocation_Problem.java
@@ -1,15 +1,16 @@
 public class Prog20_Book_Allocation_Problem {
-    static int funct1(int[] books, int students){
+    static int funct1(int[] books, int m){
+        int n = books.length;
         int maxPages=0;
-        for(int stu=1;stu<=2;stu++){
-            for(int b=0;b<books.length;b++){
+        for(int stu=1;stu<=m;stu++){
+            for(int b=0;b<n;b++){
                 
             }
         }
         return maxPages;
     }
     public static void main(String[] args) {
-        int[] books = {12,34,67,90};
+        int[] books = {25,46,28,49,24};
         int students=2;
         System.out.println(funct1(books, students));
     }
