895337843d2ad963a3af07876ffda38b29c2739d
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-06-10 21:00:07 +0530

    Added Book Allocation program Linear Search method

--- 04_Arrays and Binary Search/Binary Search/Prog20_Book_Allocation_Problem.java
+++ 04_Arrays and Binary Search/Binary Search/Prog20_Book_Allocation_Problem.java
@@ -1,17 +1,34 @@
 public class Prog20_Book_Allocation_Problem {
-    static int funct1(int[] books, int m){
-        int n = books.length;
-        int maxPages=0;
-        for(int stu=1;stu<=m;stu++){
-            for(int b=0;b<n;b++){
-                
+    static int is_Allocation_Possible1(int[] arr, int m){
+        int low = Integer.MIN_VALUE, high=0;
+        for(int el: arr){
+            low = Math.max(el,low);
+            high += el;
+        }
+        for(int maxPages=low;maxPages<=high;maxPages++){
+            int cntStudents = allocateBooks(arr, maxPages);
+            if(cntStudents==m){
+                return maxPages;
             }
         }
-        return maxPages;
+        return -1;
+    }
+    static int allocateBooks(int[] arr, int maxPages){
+        int stu=1, pagesStudent=0;
+        for(int i=0;i<arr.length;i++){
+            if(pagesStudent+arr[i]<=maxPages){
+                pagesStudent+=arr[i];
+            }
+            else{
+                stu++;
+                pagesStudent=arr[i];
+            }
+        }
+        return stu;
     }
     public static void main(String[] args) {
         int[] books = {25,46,28,49,24};
-        int students=2;
-        System.out.println(funct1(books, students));
+        int students=4;
+        System.out.println(is_Allocation_Possible1(books, students));
     }
 }
