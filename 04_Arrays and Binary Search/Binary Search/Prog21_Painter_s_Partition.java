14e92bd3ac0cd182ce7863ebf8466e90996f5d72
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-06-11 17:34:38 +0530

    Added New Binary Search program

---
+++ 04_Arrays and Binary Search/Binary Search/Prog21_Painter_s_Partition.java
@@ -0,0 +1,41 @@
+public class Prog21_Painter_s_Partition {//Painter's Partition (same solution as Book Allocation problem)
+    static int allocate_Units(int[] arr, int k){
+        int n=arr.length;
+        if(k>n) return -1;
+        int low=Integer.MIN_VALUE, high=0;
+        for(int el:arr){
+            low=Math.max(low,el);
+            high+=el;
+        }
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int cntPainters = count_Allocated_Painters(arr, mid);
+            if(cntPainters<=k){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+    static int count_Allocated_Painters(int[] arr, int maxUnits){
+        int painters=1,painterUnits=0;
+        for(int i=0;i<arr.length;i++){
+            if(painterUnits+arr[i]<=maxUnits){
+                painterUnits+=arr[i];
+            }
+            else{
+                painters++;
+                painterUnits=arr[i];
+            }
+        }
+        return painters;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40};
+        int painters=2;
+        System.out.println(allocate_Units(arr, painters));
+    }
+    
+}
