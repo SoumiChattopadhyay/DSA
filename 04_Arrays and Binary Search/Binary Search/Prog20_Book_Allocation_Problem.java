32e661edec2099ba70e99e33f2b845d5d0689bb7
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-06-11 15:27:34 +0530

    Renamed function names

--- 04_Arrays and Binary Search/Binary Search/Prog20_Book_Allocation_Problem.java
+++ 04_Arrays and Binary Search/Binary Search/Prog20_Book_Allocation_Problem.java
@@ -1,6 +1,6 @@
 public class Prog20_Book_Allocation_Problem {
     // Linear Search
-    static int is_Allocation_Possible1(int[] arr, int m){
+    static int allocateBooks1(int[] arr, int m){
         int n=arr.length;
         if(m>n) return -1;
         int low = Integer.MIN_VALUE, high=0;
@@ -9,14 +9,14 @@ public class Prog20_Book_Allocation_Problem {
             high += el;
         }
         for(int maxPages=low;maxPages<=high;maxPages++){
-            int cntStudents = allocateBooks(arr, maxPages);
+            int cntStudents = count_Students_Allocated(arr, maxPages);
             if(cntStudents==m){
                 return maxPages;
             }
         }
         return -1;
     }
-    static int allocateBooks(int[] arr, int maxPages){
+    static int count_Students_Allocated(int[] arr, int maxPages){
         int stu=1, pagesStudent=0;
         for(int i=0;i<arr.length;i++){
             if(pagesStudent+arr[i]<=maxPages){
@@ -30,7 +30,7 @@ public class Prog20_Book_Allocation_Problem {
         return stu;
     }
     // Binary Search
-    static int is_Allocation_Possible2(int[] arr, int m){
+    static int allocateBooks2(int[] arr, int m){
         int n=arr.length;
         if(m>n) return -1;
         int low = Integer.MIN_VALUE, high=0;
@@ -38,24 +38,22 @@ public class Prog20_Book_Allocation_Problem {
             low = Math.max(el,low);
             high += el;
         }
-        int ans=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            int cntStudents = allocateBooks(arr, mid);
-            if(cntStudents<=m){//if cntStudents<m means we have increased maxPages a lot so one student holds many books while another student gets none. So decrease maxPages. And if cntStudents=m means this is a possible ans so store it and look for better ans (see if we decrease maxPages more and get an ans)
-                ans = mid;
+            int cntStudents = count_Students_Allocated(arr, mid);
+            if(cntStudents<=m){//if cntStudents<m means we have increased maxPages a lot so one student holds many books while another student gets none. So decrease maxPages. And if cntStudents=m means this is a possible ans so then also look for better ans (see if we decrease maxPages more will that help us get a better ans)
                 high=mid-1;
             }
             else{//if cntStudents>m means we have decreased maxPages a lot so count of students has exceeded m. So increase maxPages.
                 low=mid+1;
             }
         }
-        return ans;
+        return low;
     }
     public static void main(String[] args) {
         int[] books = {25,46,28,49,24};
         int students=4;
-        System.out.println(is_Allocation_Possible1(books, students));
-        System.out.println(is_Allocation_Possible2(books, students));
+        System.out.println(allocateBooks1(books, students));
+        System.out.println(allocateBooks2(books, students));
     }
 }
