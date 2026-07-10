dc801aac5b054c55d96594079d16df637cbccf6f
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-07-10 18:15:17 +0530

    Added Array programs

--- 04_Arrays and Binary Search/Arrays/Prog00_Array_Imp_programs.java
+++ 04_Arrays and Binary Search/Arrays/Prog00_Array_Imp_programs.java
@@ -64,7 +64,23 @@ public class Prog00_Array_Imp_programs {
     }
     // Optimal
     static void move0sToEnd_2(int[] arr, int n){
-
+        int j=-1;
+        //make j point to the first 0
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return;//means no 0s present in the array
+        for(int i=j+1;i<n;i++){
+            if(arr[i]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
