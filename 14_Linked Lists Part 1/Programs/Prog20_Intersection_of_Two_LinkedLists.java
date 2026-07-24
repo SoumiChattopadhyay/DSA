0bd83d9ea3e9b46212f2db615021876c5d385a58
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-07-25 00:21:01 +0530

    Added & Modified programs

---
+++ 14_Linked Lists Part 1/Programs/Prog20_Intersection_of_Two_LinkedLists.java
@@ -0,0 +1,16 @@
+public class Prog20_Intersection_of_Two_LinkedLists {
+    /*
+    Approach 1: Brute Force (O(mn))
+    For every node in A, check every node in B.
+    A1 with all nodes of B
+    A2 with all nodes of B
+    ...
+    Too slow.
+    */
+/*
+
+*/
+    public static void main(String[] args) {
+       
+    }
+}
